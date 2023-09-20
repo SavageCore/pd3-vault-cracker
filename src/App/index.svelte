0e2901b7926b3d33abe29365d7ca75f47388cac1
SavageCore <talk@savagecore.uk>
2023-09-20 04:59:34 +0100

    Segmented display and general ATM keypad ish look

--- src/App/index.svelte
+++ src/App/index.svelte
@@ -5,13 +5,42 @@
 
     export const pressedNumbers = writable([]);
 
-    function handleClick(number) {
-        pressedNumbers.update(numbers => [...numbers, number]);
+    let digit1 = '';
+    let digit2 = '';
+    let digit3 = '';
+    let digit4 = '';
+
+    function handleClick(input) {
+        if (input === 'Backspace') {
+            pressedNumbers.update(numbers => numbers.slice(0, -1));
+
+            updateDisplay();
+            return;
+        }
+
+        if (input === 'OK') {
+            start();
+            return;
+        }
+
+        pressedNumbers.update(numbers => [...numbers, input]);
+
+        updateDisplay();
+
+        // If the user has entered 4 numbers, start the cracking process
+        if ($pressedNumbers.length === 4) {
+            start();
+        }
     }
 
     function start() {
         const fingerprints = $pressedNumbers;
-        console.log(fingerprints);
+
+        // Ensure that the user has entered 2, 3 or 4 numbers
+        if (fingerprints.length < 2 || fingerprints.length > 4) {
+            return;
+        }
+
         printCombinations(fingerprints);
     }
 
@@ -47,6 +76,10 @@
         let sectionCount = 0;
         let firstNumber = -1;
 
+        console.log('');
+        console.log('');
+        console.log('');
+
         for (const combo of combinations) {
             if (combo[0] !== firstNumber) {
                 firstNumber = combo[0];
@@ -55,18 +88,71 @@
             }
             console.log(`(${combo.join(', ')})`);
         }
-
-        // combinations.forEach((combo, index) => {
-        //     if (combo[0] !== firstNumber) {
-        //         firstNumber = combo[0];
-        //         sectionCount += 1;
-        //         console.log(`--- section ${sectionCount} ---`);
-        //     }
-        //     console.log(`(${combo.join(', ')})`);
-        // });
     }
 
-    printCombinations([2, 4, 8, 0]);
+    function numberToDigit(number) {
+        switch (number) {
+            case 1:
+                return 'digOne';
+            case 2:
+                return 'digTwo';
+            case 3:
+                return 'digThree';
+            case 4:
+                return 'digFour';
+            case 5:
+                return 'digFive';
+            case 6:
+                return 'digSix';
+            case 7:
+                return 'digSeven';
+            case 8:
+                return 'digEight';
+            case 9:
+                return 'digNine';
+            case 0:
+                return 'digZero';
+        }
+    }
+
+    function updateDisplay() {
+        const numbers = $pressedNumbers;
+
+        if (numbers.length === 0) {
+            digit1 = '';
+            digit2 = '';
+            digit3 = '';
+            digit4 = '';
+        }
+
+        if (numbers.length === 1) {
+            digit1 = numberToDigit(numbers[0]);
+            digit2 = '';
+            digit3 = '';
+            digit4 = '';
+        }
+
+        if (numbers.length === 2) {
+            digit1 = numberToDigit(numbers[0]);
+            digit2 = numberToDigit(numbers[1]);
+            digit3 = '';
+            digit4 = '';
+        }
+
+        if (numbers.length === 3) {
+            digit1 = numberToDigit(numbers[0]);
+            digit2 = numberToDigit(numbers[1]);
+            digit3 = numberToDigit(numbers[2]);
+            digit4 = '';
+        }
+
+        if (numbers.length === 4) {
+            digit1 = numberToDigit(numbers[0]);
+            digit2 = numberToDigit(numbers[1]);
+            digit3 = numberToDigit(numbers[2]);
+            digit4 = numberToDigit(numbers[3]);
+        }
+    }
 </script>
 
 <style src="./style.scss">
@@ -77,45 +163,101 @@
     <Menu />
     <div class="wrapper">
         <div class="content">
-            <div class="keypad">
-                <div class="row">
-                    <button class="btn" on:click={() => handleClick(1)}>
-                        1
-                    </button>
-                    <button class="btn" on:click={() => handleClick(2)}>
-                        2
-                    </button>
-                    <button class="btn" on:click={() => handleClick(3)}>
-                        3
-                    </button>
+            <div class="atm-keypad">
+                <!-- Display -->
+                <div class="display">
+                    <div class="digit {digit1}">
+                        <div class="segment segA" />
+                        <div class="segment segB" />
+                        <div class="segment segC" />
+                        <div class="segment segD" />
+                        <div class="segment segE" />
+                        <div class="segment segF" />
+                        <div class="segment segG" />
+                    </div>
+                    <div class="digit {digit2}">
+                        <div class="segment segA" />
+                        <div class="segment segB" />
+                        <div class="segment segC" />
+                        <div class="segment segD" />
+                        <div class="segment segE" />
+                        <div class="segment segF" />
+                        <div class="segment segG" />
+                    </div>
+                    <div class="digit {digit3}">
+                        <div class="segment segA" />
+                        <div class="segment segB" />
+                        <div class="segment segC" />
+                        <div class="segment segD" />
+                        <div class="segment segE" />
+                        <div class="segment segF" />
+                        <div class="segment segG" />
+                    </div>
+                    <div class="digit {digit4}">
+                        <div class="segment segA" />
+                        <div class="segment segB" />
+                        <div class="segment segC" />
+                        <div class="segment segD" />
+                        <div class="segment segE" />
+                        <div class="segment segF" />
+                        <div class="segment segG" />
+                    </div>
                 </div>
-                <div class="row">
-                    <button class="btn" on:click={() => handleClick(4)}>
-                        4
-                    </button>
-                    <button class="btn" on:click={() => handleClick(5)}>
-                        5
-                    </button>
-                    <button class="btn" on:click={() => handleClick(6)}>
-                        6
-                    </button>
-                </div>
-                <div class="row">
-                    <button class="btn" on:click={() => handleClick(7)}>
-                        7
-                    </button>
-                    <button class="btn" on:click={() => handleClick(8)}>
-                        8
-                    </button>
-                    <button class="btn" on:click={() => handleClick(9)}>
-                        9
-                    </button>
+                <!-- Keypad -->
+                <div class="keypad">
+                    <div class="row">
+                        <button class="btn" on:click={() => handleClick(1)}>
+                            1
+                        </button>
+                        <button class="btn" on:click={() => handleClick(2)}>
+                            2
+                        </button>
+                        <button class="btn" on:click={() => handleClick(3)}>
+                            3
+                        </button>
+                    </div>
+                    <div class="row">
+                        <button class="btn" on:click={() => handleClick(4)}>
+                            4
+                        </button>
+                        <button class="btn" on:click={() => handleClick(5)}>
+                            5
+                        </button>
+                        <button class="btn" on:click={() => handleClick(6)}>
+                            6
+                        </button>
+                    </div>
+                    <div class="row">
+                        <button class="btn" on:click={() => handleClick(7)}>
+                            7
+                        </button>
+                        <button class="btn" on:click={() => handleClick(8)}>
+                            8
+                        </button>
+                        <button class="btn" on:click={() => handleClick(9)}>
+                            9
+                        </button>
+                    </div>
+                    <div class="row">
+                        <button
+                            class="btn"
+                            style="color: red;"
+                            on:click={() => handleClick('Backspace')}>
+                            X
+                        </button>
+                        <button class="btn" on:click={() => handleClick(0)}>
+                            0
+                        </button>
+                        <button
+                            class="btn"
+                            style="color: green;"
+                            on:click={() => handleClick('OK')}>
+                            ✔
+                        </button>
+                    </div>
                 </div>
             </div>
-            <!-- The button to start the cracking process -->
-            <div class="start-btn">
-                <button class="btn" on:click={() => start()}>Start</button>
-            </div>
+
         </div>
     </div>
 </Layout>
