09a6fb05fd195dc31a4c7372cbbf92865cec0361
SavageCore <talk@savagecore.uk>
2023-09-23 19:50:45 +0100

    Update base url

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: '/vite-svelte-starter/',
+  base: '/pd3-vault-cracker/',
 });
