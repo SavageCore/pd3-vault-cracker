b2fcde1469ad303fcad25b0d2e8c8021950eea57
Oliver Sayers <talk@savagecore.uk>
2023-09-24 21:07:02 +0100

    Generate PWA assets with `@vite-pwa/assets-generator`

--- vite.config.ts
+++ vite.config.ts
@@ -11,7 +11,39 @@ export default defineConfig({
       devOptions: {
         enabled: true,
       },
+      manifest: {
+        name: 'PD3 Vault Cracker',
+        short_name: 'PD3 Vault Cracker',
+        description: 'A tool to generate vault combinations for Payday 3',
+        theme_color: '#2b2a33',
+        background_color: '#1c1b22',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '64x64',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: 'maskable-icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
     }),
   ],
   base: '/pd3-vault-cracker/',
+  publicDir: 'public',
 });
